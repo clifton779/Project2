89f14beafeeed761e1235654d5e21efe384da0a8
npunjani3 <73320894+npunjani3@users.noreply.github.com>
2022-03-16 17:54:35 -0400

    Add files via upload
    Sign up page containing form. Reusing same style sheet and php function file from the login branch. Sign up button redirects to login page as of now. Once form validation is created error message will appear if form is not correctly filled.

--- signup.php
+++ signup.php
@@ -1,5 +1,8 @@
 <?php
-
+    include('./common.php');
+    if(isset($_POST['signup'])) {
+        redirect_login();
+    }
 ?>
 
 <html>
@@ -8,5 +11,22 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="./style.css">
     </head>
-    <body></body>
+    <body class="sign-up">
+        <form id="sign-up" method="post" action="">
+            <h2>Sign Up Form</h2>
+            <div class="input-container">
+              <input class="input-field" type="text" placeholder="Username" name="Username">
+            </div>
+          
+            <div class="input-container">
+              <input class="input-field" type="text" placeholder="Email" name="Email">
+            </div>
+            
+            <div class="input-container">
+              <input class="input-field" type="password" placeholder="Password" name="Password">
+            </div>
+          
+            <button name="signup" type="submit" class="btn">Sign Up</button>
+          </form>
+    </body>
 </html>
