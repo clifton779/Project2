9c1a3bf70bcec0ed8b2b71735adcc5d5feb89903
npunjani3 <73320894+npunjani3@users.noreply.github.com>
2022-03-16 13:03:03 -0400

    Add files via upload
    Skeleton files for main branch to start project. The file common.php is php file for functions and reusability of html throughout all pages.

---
+++ signup.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body></body>
+</html>
