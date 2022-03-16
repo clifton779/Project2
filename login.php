c849b5962e09d7d9bab59b8a8abbbed0fdccad26
npunjani3 <73320894+npunjani3@users.noreply.github.com>
2022-03-16 17:51:04 -0400

    Add files via upload
    Login page containing form with login button and signup button. Form action is not set because we do not have home page and validation setup yet. Sign up button redirects to signup.php

--- login.php
+++ login.php
@@ -1,5 +1,8 @@
 <?php
-
+    include('./common.php');
+    if(isset($_POST['signup'])) {
+        signUp();
+    }
 ?>
 
 <html>
@@ -8,5 +11,20 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="./style.css">
     </head>
-    <body></body>
+    <body class="login">
+        <form id="login" method="post" action="">
+            <h2>Login Form</h2>
+            <div class="input-container">
+              <input class="input-field" type="text" placeholder="Username" name="Username">
+            </div>
+            
+            <div class="input-container">
+              <input class="input-field" type="password" placeholder="Password" name="Password">
+            </div>
+          
+            <button name="login" type="submit" class="btn">Login</button>
+            <hr>
+            <button name="signup" type="submit" class="btn">Sign Up</button>
+          </form>
+    </body>
 </html>
